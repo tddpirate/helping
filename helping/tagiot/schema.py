dc39daa885a645b0369b25ec4aee0eadf5228676
Omer Zak <w1@zak.co.il>
2020-04-04 06:33:11 +0300

    Finish registering all models currently defined with GraphQL.

--- helping/tagiot/schema.py
+++ helping/tagiot/schema.py
@@ -67,11 +67,49 @@ class Query(object):
     all_users = graphene.List(UserType)
     all_profiles = graphene.List(ProfileType)
 
+    all_contacttypes = graphene.List(ContactTypeType)
+    all_contacts = graphene.List(Contact1Type)
+
+    all_tasktypes = graphene.List(TaskTypeType)
+    all_tasktypeextras = graphene.List(TaskTypeExtraType)
+
+    all_needstatuss = graphene.List(NeedStatusType)
+    all_needs = graphene.List(NeedType)
+    all_needextras = graphene.List(NeedExtraType)
+
+    all_capabilitys = graphene.List(CapabilityType)
+
+
     def resolve_all_profilestatuss(self, info, **kwargs):
         return ProfileStatus.objects.all()
     def resolve_all_users(self, info, **kwargs):
         return User.objects.all()
     def resolve_all_profiles(self, info, **kwargs):
         return Profile.objects.all()
+
+    def resolve_all_contacttypes(self, info, **kwargs):
+        return ContactType.objects.all()
+    def resolve_all_contacts(self, info, **kwargs):
+        return Contact.objects.all()
+
+    def resolve_all_tasktypes(self, info, **kwargs):
+        return TaskType.objects.all()
+    def resolve_all_tasktypeextras(self, info, **kwargs):
+        return TaskTypeExtra.objects.all()
+
+    def resolve_all_needstatuss(self, info, **kwargs):
+        return NeedStatus.objects.all()
+    def resolve_all_needs(self, info, **kwargs):
+        return Need.objects.all()
+    def resolve_all_needextras(self, info, **kwargs):
+        return NeedExtra.objects.all()
+
+    def resolve_all_capabilitys(self, info, **kwargs):
+        return Capability.objects.all()
+
+#    def resolve_all_(self, info, **kwargs):
+#        return .objects.all()
+#    def resolve_all_(self, info, **kwargs):
+#        return .objects.all()
 
 # End of tagiot/schema.py
